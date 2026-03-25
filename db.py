913899ef04f8d6721e238ecf53150c643b2b0ca8
Harendra Prajapati <harendraprajapati72@gmail.com>
2026-03-25 21:00:21 +0530

    Revert "updated model and added requirements.txt"
    This reverts commit c5833211bb373ddc475ee5b3c652dd1f3b4f0a8b.

--- db.py
+++ db.py
@@ -1,80 +1,37 @@
-# import sqlalchemy
-# from datetime import datetime
-# from sqlalchemy import create_engine, Column, Integer,String,DateTime
-# from sqlalchemy.ext import declarative
-# from sqlalchemy.ext.declarative import declarative_base
-
-# Base = declarative_base()
-
-# class Image(Base):
-#     __tablename__ ='images'
-
-#     id = Column(Integer, primary_key=True)
-#     filename = Column(String)
-#     extension = Column(String)
-#     filepath = Column(String)
-#     uploader = Column(String,default='admin')
-#     created_on = Column(DateTime, default=datetime.now)
-
-#     def __str__(self):
-#         return self.filename
-
-# class Video(Base):
-#     __tablename__ ='videos'
-
-#     id = Column(Integer, primary_key=True)
-#     filename = Column(String)
-#     extension = Column(String)
-#     filepath = Column(String)
-#     uploader = Column(String,default='admin')
-#     created_on = Column(DateTime, default=datetime.now)
-
-#     def __str__(self):
-#         return self.filename
-
-# if __name__ == "__main__":
-#     engine = create_engine('sqlite:///db.sqlite3')
-#     Base.metadata.create_all(engine)
-
 import sqlalchemy
 from datetime import datetime
-from sqlalchemy import create_engine, Column, Integer, String, DateTime
+from sqlalchemy import create_engine, Column, Integer,String,DateTime
+from sqlalchemy.ext import declarative
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 class Image(Base):
-    __tablename__ = 'images'
+    __tablename__ ='images'
 
     id = Column(Integer, primary_key=True)
     filename = Column(String)
     extension = Column(String)
     filepath = Column(String)
-    uploader = Column(String, default='admin')
+    uploader = Column(String,default='admin')
     created_on = Column(DateTime, default=datetime.now)
 
     def __str__(self):
         return self.filename
 
 class Video(Base):
-    __tablename__ = 'videos'
+    __tablename__ ='videos'
 
     id = Column(Integer, primary_key=True)
     filename = Column(String)
     extension = Column(String)
     filepath = Column(String)
-    uploader = Column(String, default='admin')
+    uploader = Column(String,default='admin')
     created_on = Column(DateTime, default=datetime.now)
 
     def __str__(self):
         return self.filename
 
-# Function to create tables if they don't exist
-def init_db():
+if __name__ == "__main__":
     engine = create_engine('sqlite:///db.sqlite3')
     Base.metadata.create_all(engine)
-    return engine
-
-if __name__ == "__main__":
-    init_db()
-    print("Database tables created successfully!")
